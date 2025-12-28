e2a74562d95919abc0cbe98220b350d7f2903ae2
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-12-28 03:19:31 -0500

    refactor: restructure App component and integrate routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-social/bootstrap-social.css";
+import "font-awesome/css/font-awesome.min.css";
+
+import "typeface-lobster";
+import "typeface-open-sans";
+
+import "./styles/index.css";
+import "./styles/App.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
