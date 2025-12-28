082648e30e5e3b90eb326b10b81b4faa60756d7f
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-12-28 09:15:42 -0500

    update contact

---
+++ src/features/contact/ContactForm.jsx
@@ -0,0 +1,135 @@
+import { Button, Label, Col, FormGroup } from "reactstrap";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { validateContactForm } from "../../utils/validateContactForm"; // we'll create this next
+
+const ContactForm = () => {
+  const handleSubmit = (values, { resetForm }) => {
+    console.log("Form Values:", values);
+    console.log("JSON Format:", JSON.stringify(values));
+    alert("Thank you for your feedback!");
+    resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        phoneNum: "",
+        email: "",
+        agree: false,
+        contactType: "By Phone",
+        feedback: "",
+      }}
+      onSubmit={handleSubmit}
+      validate={validateContactForm}
+    >
+      <Form>
+        <FormGroup row>
+          <Label htmlFor="firstName" md="2">
+            First Name
+          </Label>
+          <Col md="10">
+            <Field
+              className="form-control"
+              name="firstName"
+              placeholder="First Name"
+            />
+            <ErrorMessage
+              name="firstName"
+              component="div"
+              className="text-danger"
+            />
+          </Col>
+        </FormGroup>
+
+        <FormGroup row>
+          <Label htmlFor="lastName" md="2">
+            Last Name
+          </Label>
+          <Col md="10">
+            <Field
+              className="form-control"
+              name="lastName"
+              placeholder="Last Name"
+            />
+            <ErrorMessage
+              name="lastName"
+              component="div"
+              className="text-danger"
+            />
+          </Col>
+        </FormGroup>
+
+        <FormGroup row>
+          <Label htmlFor="phoneNum" md="2">
+            Phone
+          </Label>
+          <Col md="10">
+            <Field
+              className="form-control"
+              name="phoneNum"
+              placeholder="Phone"
+            />
+            <ErrorMessage
+              name="phoneNum"
+              component="div"
+              className="text-danger"
+            />
+          </Col>
+        </FormGroup>
+
+        <FormGroup row>
+          <Label htmlFor="email" md="2">
+            Email
+          </Label>
+          <Col md="10">
+            <Field className="form-control" name="email" placeholder="Email" />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className="text-danger"
+            />
+          </Col>
+        </FormGroup>
+
+        <FormGroup row>
+          <Label check md={{ size: 4, offset: 2 }}>
+            <Field name="agree" type="checkbox" className="form-check-input" />{" "}
+            May we contact you?
+          </Label>
+          <Col md="4">
+            <Field className="form-control" name="contactType" as="select">
+              <option>By Phone</option>
+              <option>By Email</option>
+            </Field>
+          </Col>
+        </FormGroup>
+
+        <FormGroup row>
+          <Label htmlFor="feedback" md="2">
+            Your Feedback
+          </Label>
+          <Col md="10">
+            <Field
+              className="form-control"
+              name="feedback"
+              as="textarea"
+              rows="8"
+            />
+          </Col>
+        </FormGroup>
+
+        <FormGroup row>
+          <Col md={{ size: 10, offset: 2 }}>
+            <Button type="submit" color="primary">
+              Send Feedback
+            </Button>
+          </Col>
+        </FormGroup>
+      </Form>
+    </Formik>
+  );
+};
+
+export default ContactForm;
