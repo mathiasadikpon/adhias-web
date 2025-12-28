bc3bf45a38ad4c5eefece4ce876579b03cc83b4b
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-12-28 18:20:38 -0500

    refactor project descriptions and improve layout in ProjectsList component

--- src/features/projects/ProjectsList.jsx
+++ src/features/projects/ProjectsList.jsx
@@ -1,34 +1,23 @@
-import {
-  Row,
-  Col,
-  Card,
-  CardBody,
-  CardTitle,
-  CardText,
-  Button,
-} from "reactstrap";
+import { Card, CardBody, CardTitle, CardText, Col, Row } from "reactstrap";
 
 const projects = [
   {
     id: 1,
     title: "Web Calculator",
     description:
-      "Constructed a sophisticated web-based calculator with advanced functionality using JavaScript, HTML, CSS.",
-    link: "#",
+      "A sophisticated web-based calculator with advanced functionality using JavaScript, HTML, and CSS.",
   },
   {
     id: 2,
     title: "Lehman App",
     description:
-      "Contributed to the development of a comprehensive app designed to streamline course management for Lehman College students.",
-    link: "#",
+      "A student-focused app for Lehman College featuring course management, weather, and transportation integration.",
   },
   {
     id: 3,
     title: "Tip Calculator",
     description:
-      "Engineered a user-friendly mobile app capable of accurately calculating tips and splitting bills among multiple parties.",
-    link: "#",
+      "A mobile-friendly app to calculate tips and split bills accurately among multiple users.",
   },
 ];
 
@@ -36,14 +25,11 @@ const ProjectsList = () => {
   return (
     <Row className="row-content">
       {projects.map((project) => (
-        <Col md="4" className="mb-4" key={project.id}>
-          <Card>
+        <Col md="6" lg="4" key={project.id} className="mb-4">
+          <Card className="h-100 shadow-sm">
             <CardBody>
               <CardTitle tag="h5">{project.title}</CardTitle>
               <CardText>{project.description}</CardText>
-              <Button color="primary" href={project.link}>
-                View Project
-              </Button>
             </CardBody>
           </Card>
         </Col>
