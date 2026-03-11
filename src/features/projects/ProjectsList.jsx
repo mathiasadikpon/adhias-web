39b9390ca0957708e357fbe29c9c3e3621d5fcb9
Mathias Adikpon <mathiasadikpon@gmail.com>
2026-03-11 14:58:34 -0400

    update

--- src/features/projects/ProjectsList.jsx
+++ src/features/projects/ProjectsList.jsx
@@ -19,27 +19,27 @@ const projects = [
   {
     name: "JavaScript Project",
     description: "A recent JavaScript-based project demonstration.",
-    image: "jsproject.png",
+    // image: "jsproject.png",
     link: "https://youtu.be/WdXm9kuVJvI",
   },
   {
     name: "Foodie App",
     description:
       "A foodie application project with physical device demonstration.",
-    image: "foodieapp.png",
+    // image: "foodieapp.png",
     link: "https://youtu.be/MvncBR_RiZw",
   },
   {
     name: "Tip Calculator with menu",
     description:
       "Extended Tip Calculator project featuring a custom menu system.",
-    image: "tipcalc_menu.png",
+    // image: "tipcalc_menu.png",
     link: "https://youtu.be/gLP8kYH-aVI",
   },
   {
     name: "Score Counter",
     description: "A score tracking application project.",
-    image: "scorecounter.png",
+    // image: "scorecounter.png",
     link: "https://youtu.be/CY4Dir1YTS4",
   },
 
@@ -48,14 +48,14 @@ const projects = [
     name: "Data visualization cars dataset",
     description:
       "Data visualization project using a cars dataset for class demonstration.",
-    image: "dataviz.png",
+    // image: "dataviz.png",
     link: "https://youtu.be/e6auyPK6c2w",
   },
   {
     name: "Data visualization food security dataset",
     description:
       "Data visualization project using a food security dataset for class demonstration.",
-    image: "dataviz.png",
+    // image: "dataviz.png",
     link: "https://youtu.be/aQw_O99sD9Y",
   },
 
@@ -64,48 +64,48 @@ const projects = [
     name: "Mathematics: Law of Cosines Explained",
     description:
       "Educational video explaining the Law of Cosines in trigonometry.",
-    image: "law_of_cosines.png",
+    // image: "law_of_cosines.png",
     link: "https://youtu.be/Pv9DawB0cSI",
   },
   {
     name: "Factorization Degree 2 Explained",
     description:
       "Educational video explaining factorization of degree 2 polynomials.",
-    image: "factorization.png",
+    // image: "factorization.png",
     link: "https://youtu.be/RCE6ggiSYgs",
   },
   {
     name: "Mathematics: Find the Fourier Siries of Functions",
     description:
       "Exploration of finding Fourier series for various functions in mathematical analysis.",
-    image: "fourier_series.png",
+    // image: "fourier_series.png",
     link: "https://youtu.be/XLVZthT3-Os",
   },
   {
     name: "Mathematics: Orthogonal Systems of Functions",
     description:
       "Exploration of orthogonal systems of functions in mathematical analysis.",
-    image: "orthogonal.png",
+    // image: "orthogonal.png",
     link: "https://youtu.be/9s8n2Xo7l3E",
   },
   {
     name: "Mathematics: Convergence of Trigonometric Fourier Series",
     description:
       "Exploration of the convergence properties of trigonometric Fourier series.",
-    image: "convergence.png",
+    // image: "convergence.png",
     link: "https://youtu.be/B1AJw7tKG9g",
   },
   {
     name: "Trigonometric Series with Decreasing Coefficients",
     description:
       "Exploration of trigonometric series with decreasing coefficients.",
-    image: "trigonometric.png",
+    // image: "trigonometric.png",
     link: "https://youtu.be/OaS1s3OvAak",
   },
   {
     name: "Mathematics: Operations on Fourier Series",
     description: "Demonstration of mathematical operations on Fourier series.",
-    image: "fourier.png",
+    // image: "fourier.png",
     link: "https://youtu.be/etQU9sYjN3E",
   },
 ];
